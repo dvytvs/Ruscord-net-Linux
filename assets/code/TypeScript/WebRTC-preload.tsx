dc93065b4e2d37f3e45db237235b6a9644c1c676
XZY <xzy12@vk.com>
2025-09-30 14:25:31 +0500

    Update WebRTC-preload.tsx

--- assets/code/TypeScript/WebRTC-preload.tsx
+++ assets/code/TypeScript/WebRTC-preload.tsx
@@ -1,5 +1,5 @@
 const { initWebRTC } = require("./WebRTC.tsx");
 
 window.addEventListener("DOMContentLoaded", () => {
-  initWebRTC(); # Импортирует initWebRTC и вызывает её после загрузки DOMContentLoaded, то есть инициализирует WebRTC
+  initWebRTC(); // Импортирует initWebRTC и вызывает её после загрузки DOMContentLoaded, то есть инициализирует WebRTC
 });
